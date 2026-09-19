aa360cad60160a97d5605d83ee8ac5f84e44d635
UB-InfoTech <gamingnavribajar@gmail.com>
2026-09-19 19:45:30 +0530

    Update ewaybillConfig.js

--- backend/config/ewaybillConfig.js
+++ backend/config/ewaybillConfig.js
@@ -1,13 +1,4 @@
 //using
-// module.exports = {
-//     aspid: '1783712425',
-//     password: 'MakeMoney123@@',
-//     username: 'km_markana_API_123',
-//     gstin: '24AIAPM2747E1ZM',
-//     eInvPwd: 'Password@01',
-//     ewbpwd: 'Password@01',
-// };
-
 
 module.exports = {
     aspid: '1783712425',

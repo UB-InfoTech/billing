7b28c52d7f3db452dbd49a5d349b76ba2bac1f4d
UB-InfoTech <gamingnavribajar@gmail.com>
2026-09-21 17:15:51 +0530

    normalize credit note counter

--- backend/models/CreditNoteCounter.js
+++ backend/models/CreditNoteCounter.js
@@ -1,15 +1,3 @@
-const mongoose = require("mongoose");
-
-const creditNoteCounterSchema = new mongoose.Schema(
-  {
-    _id: { type: String, required: true },
-    value: { type: Number, default: 0 },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model(
-  "CreditNoteCounter",
-  creditNoteCounterSchema,
-  "credit_note_counters"
-);
+const mongoose=require("mongoose");
+const schema=new mongoose.Schema({_id:{type:String,required:true},value:{type:Number,default:0,min:0}},{timestamps:true});
+module.exports=mongoose.model("CreditNoteCounter",schema,"credit_note_counters");

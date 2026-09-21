095b7ee6778a9ef17e7f7c38bf0da1431f6caaf4
UB-InfoTech <gamingnavribajar@gmail.com>
2026-09-21 17:29:53 +0530

    use authenticated company credentials for GST lookup

--- backend/services/gstGstDetails.js
+++ backend/services/gstGstDetails.js
@@ -1,26 +1,17 @@
-//using
-const axios = require('axios');
-const { getValidToken } = require('../utils/ewaybill/tokenManager');
-const { aspid, password, username, gstin, ewbpwd } = require('../config/ewaybillConfig');
+const axios=require("axios");
+const {getValidToken}=require("../utils/ewaybill/tokenManager");
+const {aspid,password}=require("../config/ewaybillConfig");
 
-const getGstDetails = async (gstNumber) => {
-    if (!gstNumber) throw new Error('GST Number is required');
-
-    const profile = {
-        gstin: gstin,
-        eWayUserName: username,
-        eWayPassword: ewbpwd
-    };
-    
-    const token = await getValidToken(profile);
-    //in live version profile is not send ${profile.gstin}, directly {gstin} is send
-    const url = `https://gstsandbox.charteredinfo.com/ewaybillapi/dec/v1.03/Master?action=GetGSTINDetails&aspid=${aspid}&password=${password}&gstin=${profile.gstin}&username=${profile.eWayUserName}&authtoken=${token}&SearchGSTIN=${gstNumber}`;
-     try {
-        const response = await axios.get(url);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching GST details:', error);
-        throw new Error('Failed to fetch GST details');
-    }
+async function getGstDetails(gstNumber,profile){
+  const searchGSTIN=String(gstNumber||"").trim().toUpperCase();
+  if(!/^[0-9A-Z]{15}$/.test(searchGSTIN))throw new Error("A valid 15-character GSTIN is required.");
+  const gstin=profile?.gstin||process.env.TAXPRO_GSTIN;
+  const username=profile?.eWayUserName||process.env.TAXPRO_USERNAME;
+  const ewbpwd=profile?.eWayPassword||process.env.TAXPRO_EWAY_PASSWORD;
+  if(!gstin||!username||!ewbpwd||!aspid||!password)throw new Error("TaxPro GST lookup credentials are not configured.");
+  const token=await getValidToken({gstin,eWayUserName:username,eWayPassword:ewbpwd});
+  const url=(process.env.TAXPRO_BASE_URL||"https://gstsandbox.charteredinfo.com")+"/ewaybillapi/dec/v1.03/Master";
+  const response=await axios.get(url,{params:{action:"GetGSTINDetails",aspid,password,gstin,username,authtoken:token,SearchGSTIN:searchGSTIN},timeout:15000});
+  return response.data;
 }
-module.exports = { getGstDetails };
+module.exports={getGstDetails};

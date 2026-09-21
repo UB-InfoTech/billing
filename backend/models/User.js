65c55a8f2391f32940ead49c0d61712a582757f9
UB-InfoTech <gamingnavribajar@gmail.com>
2026-09-21 17:27:10 +0530

    harden user credential schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,45 +1,9 @@
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
+const mongoose=require("mongoose");
 
-// const userSchema = new mongoose.Schema({
-//   name: { type: String },
-//   email: { type: String, unique: true },
-//   password: { type: String },
-// });
+const userSchema=new mongoose.Schema({
+  username:{type:String,required:true,unique:true,trim:true,minlength:2,maxlength:80},
+  email:{type:String,required:true,unique:true,trim:true,lowercase:true,maxlength:160,index:true},
+  password:{type:String,required:true,select:false,minlength:8},
+},{timestamps:true});
 
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-//   next();
-// });
-
-// module.exports = mongoose.model('User', userSchema);
-
-
-const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true });
-
-
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-module.exports = mongoose.model('User', userSchema);
+module.exports=mongoose.model("User",userSchema);

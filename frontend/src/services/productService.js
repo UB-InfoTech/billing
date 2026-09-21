5e39ae9a237c72ab66e6d4497e2149a31b77e180
UB-InfoTech <gamingnavribajar@gmail.com>
2026-09-21 17:19:47 +0530

    centralize authenticated product API client

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,13 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'http://localhost:5000/api/products';
+const BASE_URL=(import.meta.env.VITE_API_URL||"http://localhost:5000")+"/api/products";
+const auth=()=>({headers:{"x-auth-token":localStorage.getItem("token")||""}});
 
-export const fetchProducts = () => axios.get(BASE_URL);
-
-export const createProduct = (data) =>
-  axios.post(BASE_URL, data, { headers: { 'Content-Type': 'multipart/form-data' } });
-
-export const updateProduct = (id, data) =>
-  axios.put(`${BASE_URL}/${id}`, data, { headers: { 'Content-Type': 'multipart/form-data' } });
-
-export const deleteProduct = (id) => axios.delete(`${BASE_URL}/${id}`);
+export const fetchProducts=(params={})=>axios.get(BASE_URL,{...auth(),params});
+export const createProduct=(data)=>axios.post(BASE_URL,data,{...auth(),headers:{...auth().headers,"Content-Type":"multipart/form-data"}});
+export const updateProduct=(id,data)=>axios.put(BASE_URL+"/"+id,data,{...auth(),headers:{...auth().headers,"Content-Type":"multipart/form-data"}});
+export const deleteProduct=(id)=>axios.delete(BASE_URL+"/"+id,auth());
+export const getProductById=(id)=>axios.get(BASE_URL+"/"+id,auth());
+export const searchProductByBarcode=(code)=>axios.get(BASE_URL+"/barcode/"+encodeURIComponent(code),auth());

dadb0c3c71756e41b4030955dbf44a88059165a6
UB-InfoTech <gamingnavribajar@gmail.com>
2026-09-21 19:11:32 +0530

    fix(products): handle FormData without manual content type

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -4,8 +4,8 @@ const BASE_URL=(import.meta.env.VITE_API_URL||"http://localhost:5000")+"/api/pro
 const auth=()=>({headers:{"x-auth-token":localStorage.getItem("token")||""}});
 
 export const fetchProducts=(params={})=>axios.get(BASE_URL,{...auth(),params});
-export const createProduct=(data)=>axios.post(BASE_URL,data,{...auth(),headers:{...auth().headers,"Content-Type":"multipart/form-data"}});
-export const updateProduct=(id,data)=>axios.put(BASE_URL+"/"+id,data,{...auth(),headers:{...auth().headers,"Content-Type":"multipart/form-data"}});
+export const createProduct=(data)=>axios.post(BASE_URL,data,auth());
+export const updateProduct=(id,data)=>axios.put(BASE_URL+"/"+id,data,auth());
 export const deleteProduct=(id)=>axios.delete(BASE_URL+"/"+id,auth());
 export const getProductById=(id)=>axios.get(BASE_URL+"/"+id,auth());
 export const searchProductByBarcode=(code)=>axios.get(BASE_URL+"/barcode/"+encodeURIComponent(code),auth());

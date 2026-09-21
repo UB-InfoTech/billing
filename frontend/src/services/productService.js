ef3385307c5868f44238d8f73b034e988fa7d5c7
UB-InfoTech <gamingnavribajar@gmail.com>
2026-09-21 16:11:04 +0530

    Add files via upload

---
+++ frontend/src/services/productService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+// const BASE_URL = 'http://localhost:5000/api/products';
+const BASE_URL = 'https://baba.divinesparks.in/api/products'; // Use this for production
+
+export const fetchProducts = () => axios.get(BASE_URL);
+
+export const createProduct = (data) =>
+  axios.post(BASE_URL, data, { headers: { 'Content-Type': 'multipart/form-data' } });
+
+export const updateProduct = (id, data) =>
+  axios.put(`${BASE_URL}/${id}`, data, { headers: { 'Content-Type': 'multipart/form-data' } });
+
+export const deleteProduct = (id) => axios.delete(`${BASE_URL}/${id}`);
